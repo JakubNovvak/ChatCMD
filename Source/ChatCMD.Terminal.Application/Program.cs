593627b8d66e4dc3d84d7521642e372a95c8c802
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-27 23:41:08 +0100

    Implement basic, test prompts in app's console

--- Source/ChatCMD.Terminal.Application/Program.cs
+++ Source/ChatCMD.Terminal.Application/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ChatCMD.Terminal.Infrastructure.ApiConnection;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        //Show Computer's name
+        string userName = Environment.UserName;
+
+        //Blue username in console
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write($"{userName}>");
+
+        //Regular color and user's input
+        Console.ResetColor();
+        var input = Console.ReadLine();
+        
+
+        Console.WriteLine("----------------------");
+
+
+        //Getting response
+        var response = await ApiConnection.SendMessageToAPI(false, input);
+
+        //Green color for chat's response
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("ChatCMD>");
+
+        //Regular color for text
+        Console.ResetColor();
+        Console.WriteLine($"{response}");
+
+
+        Console.WriteLine("----------------------");
+    }
+}
