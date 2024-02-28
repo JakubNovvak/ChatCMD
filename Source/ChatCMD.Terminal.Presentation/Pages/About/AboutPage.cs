566efc4b7c95c4c8912cf2b1b3257a57aecc9170
JakubNovvak <jakubszymonnowak@gmail.com>
2024-02-28 15:09:27 +0100

    Add terminal output to the Menu options.

---
+++ Source/ChatCMD.Terminal.Presentation/Pages/About/AboutPage.cs
@@ -0,0 +1,25 @@
+﻿using ChatCMD.Terminal.Presentation.TextHighlighting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatCMD.Terminal.Presentation.Pages.About
+{
+    public static class AboutPage
+    {
+        public static void PrintAbout()
+        {
+            ColorMassage.PrintColorMessage("Green", "ChatCMD", false);
+            Console.WriteLine(" is an amator project of recreating basic functionalities of the real ChatGPT ChatBot.\n" +
+                              "The Project uses OpenAI completion API with the Free Plan API Key. At this stage of\n" +
+                              "development, ChatCMD offers single chat converstaion with the language model, with\n" +
+                              "application's session based chat history."
+                );
+            ColorMassage.PrintColorMessage("Blue", "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+            ColorMassage.PrintColorMessage("Green", " ] Exit");
+        }
+    }
+}
---
+++ Source/ChatCMD.Terminal.Presentation/Pages/MainMenu/MainMenuPage.cs
@@ -0,0 +1,44 @@
+﻿using ChatCMD.Terminal.Presentation.TextHighlighting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatCMD.Terminal.Presentation.Pages.MainMenu
+{
+    public static class MainMenuPage
+    {
+        private static string[] _menuOptions { get; set; } = ["] Start Chatting", "] About ChatCMD", "] Options", "] Exit"];
+        private static string[] _menuGraphic { get; set; } = [
+            "   ________          __  ________  _______ ",
+            "  / ____/ /_  ____ _/ /_/ ____/  |/  / __ \\",
+            " / /   / __ \\/ __ `/ __/ /   / /|_/ / / / /",
+            "/ /___/ / / / /_/ / /_/ /___/ /  / / /_/ / ",
+            "\\____/_/ /_/\\__,_/\\__/\\____/_/  /_/_____/  ",
+            "\n  Your personal ChatBot in the Terminal!"
+            ];
+
+        public static void PrintMainMenu(int index)
+        {
+            ColorMassage.PrintColorMessage("Blue", "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (var graphicLine in _menuGraphic)
+                ColorMassage.PrintColorMessage("Green", graphicLine);
+
+            ColorMassage.PrintColorMessage("Blue", "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.WriteLine("Choose your option:\n");
+
+            for (int i = 0; i < _menuOptions.Length; i++)
+            {
+                if (i == index)
+                    ColorMassage.PrintColorMessage("Green", " " + _menuOptions[i]);
+                else
+                    Console.WriteLine(_menuOptions[i]);
+            }
+
+        }
+    }
+}
